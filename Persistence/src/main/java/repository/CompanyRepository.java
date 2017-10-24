51b90c656fcb134d835bfa8de7cbd9429ade821f
MarieAnneClay <marie-anne.clay@epfedu.fr>
2017-10-24 13:28:15 +0200

    first commit

---
+++ Persistence/src/main/java/repository/CompanyRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Company;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+}
