e7af01a7fef44ed0e269ee35fefeb2bfbef6aece
MarieAnneClay <marie-anne.clay@epfedu.fr>
2017-10-25 16:31:26 +0200

    [REFACTO] repositories

--- Persistence/src/main/java/repository/CompanyRepository.java
+++ Persistence/src/main/java/repository/CompanyRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import model.Company;
 
+//@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
 }
--- Persistence/src/main/java/repository/ComputerRepository.java
+++ Persistence/src/main/java/repository/ComputerRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import model.Computer;
 
+//@Repository
 public interface ComputerRepository extends JpaRepository<Computer, Long> {
 
     long countByNameContainingOrCompanyNameContaining(String name, String companyName);
 
-    Page<Computer> findAllComputersByNameContainingOrCompanyNameContaining(Pageable page, String name, String companyName); // TODO sort+limit
+    Page<Computer> findAllComputersByNameContainingOrCompanyNameContaining(Pageable page, String name, String companyName);
 
 }
