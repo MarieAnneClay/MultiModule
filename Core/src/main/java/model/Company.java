5b081ecf1cb6076a3407fd3d70b6b81fd5883ce4
MarieAnneClay <marie-anne.clay@epfedu.fr>
2017-11-07 11:25:37 +0100

    prez

--- Core/src/main/java/model/Company.java
+++ Core/src/main/java/model/Company.java
@@ -1,11 +1,18 @@
 package model;
 
-//@Entity
-//@Table(name = "company")
-//@XmlRootElement
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "company")
+@XmlRootElement
 public class Company {
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
--- Core/src/main/java/model/Computer.java
+++ Core/src/main/java/model/Computer.java
@@ -2,27 +2,39 @@ package model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
 
+import org.hibernate.annotations.Formula;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters.LocalDateConverter;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
-//@Entity
-//@Table(name = "computer")
-//@XmlRootElement
-// @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@Entity
+@Table(name = "computer")
+@XmlRootElement
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class Computer {
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Size(min = 1, max = 60)
     @NonNull
@@ -40,8 +52,20 @@ public class Computer {
     @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate discontinued;
     @Nullable
-    // @Column(name = "company_id")
+    @Column(name = "company_id")
     private Long companyId;
+    @JsonIgnore
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @Formula("(SELECT c.name FROM company c WHERE c.id = company_id)")
+    private String companyName;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 
     /** DEFAULT CONSTRUCTOR. */
     public Computer() {
@@ -95,43 +119,6 @@ public class Computer {
         this.introduced = introduced;
         this.discontinued = discontinued;
     }
-
-    // public static class ComputerBuilder {
-    // public Computer computer;
-    //
-    // public ComputerBuilder() {
-    // this.computer = new Computer();
-    // }
-    //
-    // public ComputerBuilder setId(Long id) {
-    // this.computer.setId(id);
-    // return this;
-    // }
-    //
-    // public ComputerBuilder setName(String name) {
-    // this.computer.setName(name);
-    // return this;
-    // }
-    //
-    // public ComputerBuilder setIntroduced(LocalDate introduced) {
-    // this.computer.setIntroduced(introduced);
-    // return this;
-    // }
-    //
-    // public ComputerBuilder setDiscontinued(LocalDate discontinued) {
-    // this.computer.setDiscontinued(discontinued);
-    // return this;
-    // }
-    //
-    // public ComputerBuilder setLongId(Long companyId) {
-    // this.computer.setCompanyId(companyId);
-    // return this;
-    // }
-    //
-    // public Computer build() {
-    // return this.computer;
-    // }
-    // }
 
     /* GETTERS & SETTERS */
     public Long getId() {
